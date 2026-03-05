e5a7d67032c7ba36543e72d144b55a666179e73a
Elaiza Jane Moreno <Elaizajanemoreno3@gmail.com>
2026-03-05 14:25:31 +0800

    Added frontend and backend

---
+++ backend/src/main/java/com/dthoughts/g4/moreno/MorenoApplication.java
@@ -0,0 +1,13 @@
+package com.dthoughts.g4.moreno;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MorenoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MorenoApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/dthoughts/g4/moreno/MorenoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dthoughts.g4.moreno;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MorenoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
