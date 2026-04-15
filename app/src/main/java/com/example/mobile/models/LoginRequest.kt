5d205606d7dd55fa43943bf087670d98ba673c2a
Elai <Elaizajanemoreno3@gmail.com>
2026-04-15 15:09:53 +0800

    Delete app/src/main directory

--- app/src/main/java/com/example/mobile/models/LoginRequest.kt
+++
@@ -1,6 +0,0 @@
-package com.example.mobile.ui.theme
-
-data class LoginRequest(
-    val email: String,
-    val password: String
-)
--- app/src/main/java/com/example/mobile/models/RegisterRequest.kt
+++
@@ -1,8 +0,0 @@
-package com.example.mobile.ui.theme
-
-data class RegisterRequest(
-    val email: String,
-    val password: String,
-    val firstName: String,
-    val lastName: String
-)
